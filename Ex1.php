d12a8ae443ea4ebedb6a8f5cdbac1c358687773d
Trinh Son <caoson.trinh@gmail.com>
2018-02-01 19:59:40 +0200

    Excercise 1

---
+++ Ex1.php
@@ -0,0 +1,51 @@
+<?php
+
+class Model
+{
+    public $string;
+
+    public function __construct(){
+        $this->string = "Hello World!";
+    }
+
+}
+
+class View
+{
+    private $model;
+    private $controller;
+
+    public function __construct($controller,$model) {
+        $this->controller = $controller;
+        $this->model = $model;
+    }
+
+    public function output() {
+        return "<a href='Ex1.php?action=clicked'>" . $this->model->string . "</a>";
+    }
+}
+
+class Controller
+{
+    private $model;
+
+    public function __construct($model){
+        $this->model = $model;
+    }
+
+    public function clicked() {
+        $this->model->string = "Updated Hello World!";
+    }
+}
+
+$model = new Model();
+$controller = new Controller($model);
+$view = new View($controller, $model);
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+}
+
+echo $view->output();
+
+ ?>
